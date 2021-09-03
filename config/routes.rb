c70bd82fcd46ef9d78fdcbf41c9d4ac83afd2d2b
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-09-03 10:12:25 +0200

    Add restricted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :measurements
   resources :goals
-  resources :coins
-  resources :users
+  resources :coins, only: [:create, :index]
+  resources :users, only [:create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
