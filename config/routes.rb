743faee73ae816904c702d35395154ee32fcb99a
Sergio Cortes <sergiocortessat@gmail.com>
2021-09-03 14:40:00 +0200

    Add correciton routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :measurements
   resources :goals
   resources :coins, only: [:create, :index]
-  resources :users, only [:create, :show, :index]
+  resources :users, only: [:create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
